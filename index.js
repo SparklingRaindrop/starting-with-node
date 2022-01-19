403df24037d18db227ce2e20bd260568ba265646
Tsubasa <92432492+SparklingRaindrop@users.noreply.github.com>
2022-01-19 10:52:00 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+import { shuffle, take } from "lodash";
+
+const ingredients = [
+    "Banana", 
+    "Apple", 
+    "Blueberries", 
+    "Brocoli", 
+    "Orange", 
+    "Almond Milk", 
+    "Spinach", 
+    "Strawberries", 
+    "Carrot", 
+    "Ginger", 
+    "Avocado", 
+    "Peaches", 
+    "Frozen Mango"
+];
+
+const button = document.querySelector('button');
+const ul = document.querySelector('ul');
+
+function shuffleAndRender() {
+    // How can we shuffle?
+    
+    // Shuffle and render 5 ingredients
+    const shuffledIngredients = shuffle(ingredients);
+    const fiveIngredients = take(shuffledIngredients, 10);
+
+    fiveIngredients.forEach((ingredient) => {
+        const li = document.createElement('li');
+        li.textContent = ingredient;
+        ul.append(li);
+    });
+
+}
+
+button.addEventListener("click", () => {
+    /* This empties the list */
+    ul. textContent = null;
+    shuffleAndRender();
+});
+
+console.log("yooo looo smoothies");
